320b685960439f33438d3a01bff2c059cfe7a79e
Shelley Dignum <shelleydignum@gmail.com>
2015-02-28 11:22:41 -0500

    add form event listener

---
+++ js/main.js
@@ -0,0 +1,13 @@
+var theButton = document.getElementById ( 'button' );
+
+theButton.addEventListener( "click", function() {
+	var firstName = document.getElementById( "fname" ).value;
+	var lastName = document.getElementById( "lname" ).value;
+	var email = document.getElementById( "email" ).value;
+	var message = document.getElementById( "msg" ).value;
+
+	console.log( 'The firstName is: ' + firstName);
+	console.log( 'The lastName is: ' + lastName);
+	console.log( 'The email is: ' + email);
+	console.log( 'The message is: ' + message);
+});
