7a49a22238170c09a3388ad6dc91f080f7811162
Shelley Dignum <shelleydignum@gmail.com>
2015-02-28 09:33:43 -0500

    add counter to newPara

--- js/main.js
+++ js/main.js
@@ -1,9 +1,17 @@
 var theHeading = document.getElementsByTagName("h1");
 
+var click = 1;
+window.onclick = function () {
+    click++;
+    return click;
+}
+
 theHeading[0].addEventListener( "click", function() {
 	var newPara = document.createElement( "p" );
-	newPara.innerHTML = "This is click number";
+
+	newPara.innerHTML = "This is click number " + click;
 
 	var theContainer = document.getElementsByTagName("body")[0];
 	theContainer.appendChild( newPara );
 });
+
